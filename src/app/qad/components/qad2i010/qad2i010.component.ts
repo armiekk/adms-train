2cb5a4215276ef821206583f9b49da205a04fb25
prin.j <prin.j@cdg.co.th>
2016-11-24 08:28:02 +0700

    update qad1 24/11/2016

---
+++ src/app/qad/components/qad2i010/qad2i010.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qad2i010',
+  templateUrl: './qad2i010.component.html',
+  styleUrls: ['./qad2i010.component.css']
+})
+export class Qad2i010Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
