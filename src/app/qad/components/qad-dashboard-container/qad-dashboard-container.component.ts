c2744d81e12d21f46bd7e034976114d51d6bc361
armst0910 <nongarmza@gmail.com>
2016-11-30 16:28:11 +0700

    update qad route

--- src/app/qad/components/qad-dashboard-container/qad-dashboard-container.component.ts
+++ src/app/qad/components/qad-dashboard-container/qad-dashboard-container.component.ts
@@ -32,42 +32,42 @@ export class QadDashboardContainerComponent implements OnInit {
       {
         moduleName: 'CMMI DOCUMENT',
         subProgram: [
-          { name: 'CMMI Document', id: 'QAD2I010', uri: 'QAD2I010' },
-          { name: 'CMMI Document History', id: 'QAD2Q010', uri: 'QAD2Q010' },
-          { name: 'CMMI Document', id: 'QAD2R010', uri: 'QAD2R010' },
+          { name: 'CMMI Document', id: 'QAD2I010', uri: '/qad/QAD2I010' },
+          { name: 'CMMI Document History', id: 'QAD2Q010', uri: '/qad/QAD2Q010' },
+          { name: 'CMMI Document', id: 'QAD2R010', uri: '/qad/QAD2R010' },
         ]
       },
       {
         moduleName: 'QA CHECKLIST',
         subProgram: [
-          { name: 'QA Checklist', id: 'QAD3I010', uri: 'QAD3I010' },
-          { name: 'Checklist Search', id: 'QAD3Q010', uri: 'QAD3Q010' },
-          { name: 'CMMI Document', id: 'QAD3R010', uri: 'QAD3R010' },
+          { name: 'QA Checklist', id: 'QAD3I010', uri: '/qad/QAD3I010' },
+          { name: 'Checklist Search', id: 'QAD3Q010', uri: '/qad/QAD3Q010' },
+          { name: 'CMMI Document', id: 'QAD3R010', uri: '/qad/QAD3R010' },
         ]
       },
       {
         moduleName: 'Audit PROGRAMS',
         subProgram: [
-          { name: 'Audit Programs', id: 'QAD4I010', uri: 'QAD4I010' },
-          { name: 'Audit Programs Search', id: 'QAD4Q010', uri: 'QAD4Q010' },
-          { name: 'CMMI Document', id: 'QAD4R010', uri: 'QAD4R010' },
+          { name: 'Audit Programs', id: 'QAD4I010', uri: '/qad/QAD4I010' },
+          { name: 'Audit Programs Search', id: 'QAD4Q010', uri: '/qad/QAD4Q010' },
+          { name: 'CMMI Document', id: 'QAD4R010', uri: '/qad/QAD4R010' },
         ]
       },
       {
         moduleName: 'QA REPORT',
         subProgram: [
-          { name: 'Audit Programs', id: 'QAD2R010', uri: 'QAD2R010' },
-          { name: 'Audit Programs Search', id: 'QAD2Q010', uri: 'QAD2Q010' },
+          { name: 'Audit Programs', id: 'QAD2R010', uri: '/qad/QAD2R010' },
+          { name: 'Audit Programs Search', id: 'QAD2Q010', uri: '/qad/QAD2Q010' },
           { name: 'QA Metrics', id: '', uri: '' },
           { name: 'QA Audit Project', id: '', uri: '' },
-          { name: 'QA Summary Checklist', id: 'QAD3R010', uri: 'QAD3R010' },
+          { name: 'QA Summary Checklist', id: 'QAD3R010', uri: '/qad/QAD3R010' },
         ]
       },
       {
         moduleName: 'QA MASTER',
         subProgram: [
           { name: 'QA MENU', id: '', uri: '' },
-          { name: 'QA Activities', id: 'QAD0Q010', uri: 'QAD0Q010' },
+          { name: 'QA Activities', id: 'QAD0Q010', uri: '/qad/QAD0Q010' },
           { name: 'Work Category', id: '', uri: '' },
           { name: 'CMMI Document (Document, BI)', id: '', uri: '' },
           { name: 'Checklist Details', id: '', uri: '' },
